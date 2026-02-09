d17b48c8e5dba2eebafab6550eba126fcede053d
khalid-yarka <khaalidyarka@gmail.com>
2026-02-09 14:26:03 +0300

    Start from zero of ardayda bot

--- bots/ardayda_bot/handlers.py
+++ bots/ardayda_bot/handlers.py
@@ -15,13 +15,13 @@ def validate_field(field, content):
     """Validate user name"""
     
     if field == "name":
-        if len(name.split()) < 3:
+        if len(content.split()) < 3:
             return False, "Fadlan Magacaga oo 3addexan gali."
-        if len(name) > 30:
+        if len(content) > 30:
             return False, "Iska hubi magaca !"
         
         return True, "Valid Name [✓]"
-    elif check == "school":
+    elif field == "school":
         return True, "good"
         
 
@@ -37,15 +37,22 @@ def complate_regestering(bot, message, name=False, school=False, class_=False):
         
         
     column = "name" if name else "school" if school else "class" if class_ else None
-    next_step = "school" if name else "class" if school else "complate" if class_
+    next_step = "school" if name else "class" if school else "complate" if class_ else None
     
     Validity = validate_field(field=column , content=text)
     
     # invalid types
     if not Validity[0]:
-        bot.reply_to(mesaage, Validity[1])
+        bot.reply_to(message, Validity[1])
     else:
         set_coulmn(id_=user_id, column=name, value=text,next_step=next_step)
         respond = "[✓] you've done you loging.\n\n/help - for helping..." if next_step == "complate" else "[!] let's to the next step.\n\nEnter Your {next_step.capital()} ↓"
-        bot.reply_to(mesaage, respond)
+        bot.reply_to(message, respond)
         
+        
+        
+        
+#----------------------- Message handlers -----------------------#
+def send_main_menu(bot,message):
+    user_id = message.from_user.id
+    bot.reply_to(message, message.text)
